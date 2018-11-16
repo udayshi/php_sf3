ee840a368009270341b12945626582ddb7d1ba30
Uday Shiwakoti <uday.shiwakoti@mullenlowegroup.com>
2018-11-16 15:39:39 +0000

    Services added

--- app/src/AppBundle/Service/DemoService.php
+++ app/src/AppBundle/Service/DemoService.php
@@ -1,20 +1,9 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: uday.shiwakoti
- * Date: 16/11/2018
- * Time: 15:22
- */
-
 namespace AppBundle\Service;
-
-
-use AppBundle\Entity\Car;
-
 class DemoService
 {
     public function helloService($name){
-        return 'Car Make: '.$name;
+        return 'Hello: '.$name;
 
     }
 }
